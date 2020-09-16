70aeb0eb289115a8454c0524f08e08f71a549b59
Aronya Baksy <abaksy@gmail.com>
2020-09-16 19:52:03 +0530

    Create deadlock_detect.cpp

---
+++ deadlock_detect.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+
+int main()
+{
+    int m = 10; //Number of available requests
+    int n = 5;  //Number of processes
+    std::vector<int> available;
+    std::vector<std::vector<int>> allocation;
+    std::vector<std::vector<int>> request;
+
+    
+}
