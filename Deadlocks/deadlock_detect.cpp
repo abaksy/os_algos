aa11887d8345b2bfb1e7e5d1315f217479a9a097
Aronya Baksy <50787681+abaksy@users.noreply.github.com>
2020-09-17 09:13:50 +0530

    Code organization

---
+++ Deadlocks/deadlock_detect.cpp
@@ -0,0 +1,82 @@
+#include "deadlock_utils.h"
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+
+//Returns an index i such that finish[i]==false and request[i] <= work
+std::vector<int> find_index(std::vector<bool>& finish, std::vector<std::vector<int>>& request, std::vector<int>& work);
+
+//Detect deadlock using the finish vector. No deadlocks if finish[i] is true for all i
+void detect_deadlock(std::vector<bool>& finish);
+
+ 
+int main()
+{
+    int m = 3;  //Number of resource classes
+    int n = 5;  //Number of processes
+    std::vector<int> available;
+    std::vector<std::vector<int>> allocation;
+    std::vector<std::vector<int>> request;
+    int temp;
+    read_vec(available, m);
+    read_2D(allocation, n, m);
+    read_2D(request, n, m);
+    std::vector<int> work = available;
+    std::vector<bool> finish;
+    for(int i=0; i<n; ++i)
+    {
+        finish.push_back(iszero(allocation[i]));
+    }
+    int ctr = 1;
+    while(true)
+    {
+        std::cout<<"Iteration "<<ctr<<"\n";
+        ctr++;
+        std::vector<int> indices = find_index(finish, request, work);
+        if(indices.size() == 0) //No indices found
+            break;
+        else
+        {
+            int ind = indices[0];
+            add_vecs(work, allocation[ind]);
+            finish[ind] = true;
+        }
+    }
+    detect_deadlock(finish);
+    return 0;
+}
+
+std::vector<int> find_index(std::vector<bool>& finish, std::vector<std::vector<int>>& request, std::vector<int>& work)
+{
+    std::set<int> s1, s2;
+    for(int i=0; i<request.size(); ++i)
+    {
+        if(compare_vec(request[i], work))
+            s1.insert(i);
+    }
+    for(int i=0; i<finish.size(); ++i)
+    {
+        if(finish[i] == false)
+           s2.insert(i);
+    }
+    std::vector<int> result(5);
+    std::vector<int>::iterator it = std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), result.begin());
+    result.resize(it - result.begin());
+    return result;
+}
+
+void detect_deadlock(std::vector<bool>& finish)
+{
+    bool flag = true;
+    for(int i=0;i<finish.size(); ++i)
+    {
+        if(finish[i]==false)
+        {
+            flag = false;
+            std::cout<<"Deadlock detected in process "<<i<<"\n";
+        }
+    }
+    if(flag)
+        std::cout<<"No deadlocks detected!\n";
+}
